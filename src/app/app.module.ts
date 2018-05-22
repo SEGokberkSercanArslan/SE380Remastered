b44549f765a02fa4379f24a3aa25e2f61db7d896
Sercan.PC <se-sercan@outlook.com>
2018-05-22 16:56:14 +0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import {LogInPage} from "../pages/log-in/log-in";
+import {SignUpPage} from "../pages/sign-up/sign-up";
+import {ForgetPasswordPage} from "../pages/forget-password/forget-password";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LogInPage,
+    SignUpPage,
+    ForgetPasswordPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LogInPage,
+    SignUpPage,
+    ForgetPasswordPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/log-in/log-in.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {SignUpPage} from "../sign-up/sign-up";
+import {ForgetPasswordPage} from "../forget-password/forget-password";
+
+/**
+ * Generated class for the LogInPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-log-in',
+  templateUrl: 'log-in.html',
+})
+export class LogInPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LogInPage');
+  }
+
+  navigateSignUpPage(){
+    this.navCtrl.push(SignUpPage);
+  }
+
+  navigateForgetPasswordPage(){
+    this.navCtrl.push(ForgetPasswordPage);
+  }
+
+}
