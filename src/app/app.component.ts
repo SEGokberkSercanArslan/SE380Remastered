84918883cf2bda0d6b9f1f7cd75a9f8b535fc367
Doruk Akdas <37660515+DorukAkdas@users.noreply.github.com>
2018-05-31 00:27:08 +0300

    Sercaninki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import {RecipeListPage} from "../pages/recipe-list/recipe-list";
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
