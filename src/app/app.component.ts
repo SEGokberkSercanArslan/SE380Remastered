c4b61f127e80dceae420d7e751db2881793cdc58
macbookproretina <se-sercan@outlook.com>
2018-05-30 13:56:50 +0300

    -Recipe Page Fab Button Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,15 @@ import {SignUpPage} from "../pages/sign-up/sign-up";
 import {ForgetPasswordPage} from "../pages/forget-password/forget-password";
 import {TabsPage} from "../pages/tabs/tabs";
 import {AddStagePage} from "../pages/add-stage/add-stage";
+import {MyFavoriteRecipesPage} from "../pages/my-favorite-recipes/my-favorite-recipes";
+import {RecipeListPage} from "../pages/recipe-list/recipe-list";
 @Component({
   templateUrl: 'app.html'
 })
 
 
-
 export class MyApp {
-  rootPage:any = AddStagePage;
+  rootPage:any = RecipeListPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
