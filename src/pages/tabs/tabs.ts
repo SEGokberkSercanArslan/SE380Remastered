1cfe3e18f602598aeb0d21f162ec49e230163cf7
Kerimcan Uyanık <uyanik.kerimcan@gmail.com>
2018-05-31 19:25:30 +0300

    Firebase Authentication v1.5

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams, ToastController} from "ionic-angular";
 
 import {RecipeListPage} from "../../../../SE380Remastered/src/pages/recipe-list/recipe-list";
 import {MyFavoriteRecipesPage} from "../../../../SE380Remastered/src/pages/my-favorite-recipes/my-favorite-recipes";
 import {HomePage} from "../../../../SE380Remastered/src/pages/home/home";
 
-import {AngularFireAuth} from "angularfire2/auth";
 
-@IonicPage()
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -19,24 +16,7 @@ export class TabsPage {
   tab3Root: any = MyFavoriteRecipesPage;
 
 
-  constructor(private afAuth: AngularFireAuth, private toast: ToastController, public navCtrl: NavController, public navParams: NavParams) {
+  constructor() {
 
-  }
-
-  ionViewWillLoad() {
-    this.afAuth.authState.subscribe(data => {
-      if(data && data.email && data.uid) {
-        this.toast.create({
-          message: 'Welcome to Cook Organizer, ${data.email}',
-          duration: 3000
-        }).present();
-      }
-      else {
-        this.toast.create({
-          message: 'Could not find authentication details.',
-          duration: 3000
-        }).present();
-      }
-    });
   }
 }
